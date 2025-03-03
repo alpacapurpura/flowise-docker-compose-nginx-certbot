d84251e1ae385b8beac544f8a214e87d28e7b9f4
Christian <alpacapurpura@gmail.com>
2025-03-03 13:01:25 -0500

    Actualizaciones finales 1

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Procesar variables en plantilla
-envsubst '${DOMAIN} ${FLOWISE_PORT}' < /etc/nginx/templates/nginx.conf > /etc/nginx/nginx.conf
+envsubst '${DOMAIN} ${FLOWISE_PORT} ${FLOWISE_USERNAME} ${FLOWISE_PASSWORD}' < /etc/nginx/templates/nginx.conf.template > /etc/nginx/nginx.conf
 
 # Ejecutar el comando original de Nginx
 exec "$@"
